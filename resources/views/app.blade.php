809982af9b8554e536eae9d514ed7e5b15b9daab
yonezawa051319 <yonezawa051319@gmail.com>
2021-09-30 23:40:15 +0000

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Vue Laravel SPA') }}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+        
+    </head>
+    <body>
+        <div id="app">
+            
+            <header-component></header-component>
+            <example-component></example-component>
+            
+            
+        </div>
+        
+        <!-- Script -->
+        <script src="{{ mix('/js/app.js') }}" defer></script>
+    </body>
+</html>
