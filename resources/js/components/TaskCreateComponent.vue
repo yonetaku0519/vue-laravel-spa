bd9f697e467aeb1398c1d9c74cbdbdafb25a8ead
yonezawa051319 <yonezawa051319@gmail.com>
2021-10-02 23:37:03 +0000

    Create ready

--- resources/js/components/TaskCreateComponent.vue
+++ resources/js/components/TaskCreateComponent.vue
@@ -2,18 +2,18 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-sm-6">
-                <form>
+                <form v-on:sublit.prevemt="submit">
                     <div class="form-group row">
                         <label for="title" class="col-sm-3 col-form-label">Title</label>
-                        <input type="text" class="col-sm-9 form-control" id="title">
+                        <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title">
                     </div>
                     <div class="form-group row">
                         <label for="content" class="col-sm-3 col-form-label">Content</label>
-                        <input type="text" class="col-sm-9 form-control" id="content">
+                        <input type="text" class="col-sm-9 form-control" id="content" v-model="task.content">
                     </div>
                     <div class="form-group row">
                         <label for="person-in-change" class="col-sm-3 col-form-label">Parson In Change</label>
-                        <input type="text" class="col-sm-9 form-control" id="person-in-change">
+                        <input type="text" class="col-sm-9 form-control" id="person-in-change" v-model="task.person-in-change">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
@@ -23,5 +23,19 @@
 </template>
 
 <script>
-export default {}
+export default {
+    data: function() {
+        return {
+            task:{}
+        }
+    },
+    methods: {
+        submit() {
+            axios.post('api/tasks', this.task)
+                .then((res) => {
+                    this.$router.push({name: 'task.list'});
+                });
+        }
+    }
+}
 </script>
