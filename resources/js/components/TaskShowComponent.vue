935d8a117c937aa53feba3787e0d931f72553c40
yonezawa051319 <yonezawa051319@gmail.com>
2021-10-02 23:30:56 +0000

    List Show ready

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -29,6 +29,22 @@
 export default {
     props: {
         taskId: String
+    },
+    data: function () {
+        return {
+            task: {}
+        }
+    },
+    methods: {
+        getTask() {
+            axios.get('/api/tasks/' + this.taskId)
+                .then((res) => {
+                    this.task = res.data;
+                });
+        }
+    },
+    mounted() {
+        this.getTask();
     }
 }
 </script>
