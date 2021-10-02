abe1228715fe4fdb0405ac1569435108fad4018d
yonezawa051319 <yonezawa051319@gmail.com>
2021-10-02 23:41:49 +0000

    Edit ready

--- resources/js/components/TaskEditComponent.vue
+++ resources/js/components/TaskEditComponent.vue
@@ -2,22 +2,22 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-sm-6">
-                <form>
+                <form v-on:submit.prevent="submit">
                     <div class="form-group row">
                         <label for="id" class="col-sm-3 col-form-label">ID</label>
-                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-bind:value="taskId">
+                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-model="task.id">
                     </div>
                     <div class="form-group row">
                         <label for="title" class="col-sm-3 col-form-label">Title</label>
-                        <input type="text" class="col-sm-9 form-control" id="title">
+                        <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title">
                     </div>
                     <div class="form-group row">
                         <label for="content" class="col-sm-3 col-form-label">Content</label>
-                        <input type="text" class="col-sm-9 form-control" id="content">
+                        <input type="text" class="col-sm-9 form-control" id="content" v-model="task.content">
                     </div>
                     <div class="form-group row">
                         <label for="person-in-change" class="col-sm-3 col-form-label">Parson In Change</label>
-                        <input type="text" class="col-sm-9 form-control" id="person-in-change">
+                        <input type="text" class="col-sm-9 form-control" id="person-in-change" v-model="task.person_in_change">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
